6961dc2b7024f5b2a79453128f37a9d5ef245853
PeterPNBF <yuhaimingbj@gmail.com>
2016-06-02 17:21:54 +0800

    Initial commit, ubuntu16 install steps

---
+++ operatorinstalllog.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+copy hosts
+chrome sudo apt install google-chrome-stable installpath /usr/bin/google-chrome-stable  google install helperurl=http://jingyan.baidu.com/article/335530da98061b19cb41c31d.html cp ~/.config/google-chrome-stable .
+firefox bookmarks=>unsorted bookmarks=>import and backup
+virtualbox sudo apt install virtualbox cp -r old~/VirtualBox\ Vms ~/ && cp -r old~/.config/virtualbox ~/.config/
+sqldeveloper sudo apt install sqldeveloper-package
+sudo cp -r /media/lord/Seagate\ Backup\ Plus\ Drive/ubuntu14.04/1941b173-6fad-4a7f-8bd3-643364f308f1/usr/lib/jvm /usr/lib/ cp old/etc/profile  /etc/profile chown -R lord:lord /etc/lib/jvm sudo update-alternatives --install /usr/bin/javac javac /usr/lib/jvm/jdk1.7.0_80/bin/javac 300 sudo update-alternatives --install /usr/bin/java java /usr/lib/jvm/jdk1.7.0_80/bin/java 300 sudo update-alternatives --config java sudo update-alternatives --config javac
+maven sudo cp -r /media/lord/Seagate\ Backup\ Plus\ Drive/ubuntu14.04/1941b173-6fad-4a7f-8bd3-643364f308f1/usr/lib/apache-maven-3.2.1 /usr/lib/ chown source /etc/profile source ./bashrc for colour
+sudo apt-get install mysql-server mysql-client lord  http://blog.csdn.net/dazhi_100/article/details/41980959 sudo apt-get install mysql-workbench
+sqldeveloper http://askubuntu.com/questions/458554/how-to-install-sql-developer-on-ubuntu-14-04
+ideaIU sudo tar -xvf ideaIU/ideaIU-2016.1.2b.tar -C /opt/
